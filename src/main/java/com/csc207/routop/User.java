59f66672867208ee5910fc885269b1c6b7531f99
laviealon <79278018+laviealon@users.noreply.github.com>
2021-11-13 20:59:46 -0500

    Fixed bug in TaskSerializable

--- src/main/java/com/csc207/routop/User.java
+++ src/main/java/com/csc207/routop/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    @Column(name= "id", nullable = false)
+    @Column(name= "bigint", nullable = false)
     private double id;
 
     @Column(name = "username")
