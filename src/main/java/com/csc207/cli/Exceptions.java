e99a6300d0c13b29476f001e80578ed61247626c
Jeong Min Hwang <jeongmin.hwang@mail.utoronto.ca>
2021-11-14 23:11:27 -0500

    modified to throw and catch exceptions

--- src/main/java/com/csc207/cli/Exceptions.java
+++ src/main/java/com/csc207/cli/Exceptions.java
@@ -5,7 +5,6 @@ public class Exceptions {
     /**
      * This class holds all the exceptions
      */
-    public static final String FULL_SCHEDULE = "There is no space for this task in the schedule";
     public static final String INVALID_DATE_FORMAT = "The format of the day is incorrect";
     public static final String SCHEDULE_CONFLICT = "The two tasks have conflicting times";
     public static final String INVALID_TIME_FORMAT = "The format of the time is incorrect";
