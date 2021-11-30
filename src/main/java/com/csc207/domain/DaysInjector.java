8a8b3426a736742542bf42db5dcc0a0662d3c30f
laviealon <79278018+laviealon@users.noreply.github.com>
2021-11-29 21:44:33 -0500

    Added documentation to the DaysInjector.java class and merged the dependency injection pull request manually (as the merge was to complex to do from the GitHub web interface).

--- src/main/java/com/csc207/domain/DaysInjector.java
+++ src/main/java/com/csc207/domain/DaysInjector.java
@@ -2,7 +2,17 @@ package com.csc207.domain;
 
 import java.time.LocalDate;
 
+/**
+ * A class which constructs a list of seven consecutive Day objects to be
+ * injected into the Week constructor.
+ */
 public class DaysInjector {
+    /**
+     * Returns a list of seven consecutive Day objects.
+     *
+     * @param startDate: the LocalDate of the first Day object.
+     * @return: a list of seven consecutive Day objects.
+     */
     public static Day[] constructDayList(LocalDate startDate){
         Day[] days = new Day[7];
         for(int i = 0; i <= 6; i++){
