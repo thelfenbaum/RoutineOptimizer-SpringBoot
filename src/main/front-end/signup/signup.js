77e1a88bdca1ab165b5c6b1bd869b817a5ac0f0d
thelf <thelfenbaum@gmail.com>
2021-12-07 17:02:19 -0500

    we added methods to signup.js to handle to check if username in database and allow user to sign up with chosen username if not

--- src/main/front-end/signup/signup.js
+++ src/main/front-end/signup/signup.js
@@ -1,10 +1,11 @@
 const API_BASE_URL = "http://localhost:8080/users"
 
+let triedUsername = document.getElementById("username").value;
 let myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
 let raw = JSON.stringify({
-    "username": document.getElementById("username").value,
+    "username": triedUsername,
     "password": document.getElementById("password").value
 });
 
@@ -21,6 +22,30 @@ async function signUp(){
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 }
+
+
+// takes username and returns userId. Returns undefined if username not in database.
+async function getUserId(username){
+     return fetch(API_BASE_URL + username)
+            .then(response => response.json())
+            .then(user => user.id)
+            // reject is a good thing here. this is when we want to let them use the username
+            .catch(reject => console.log(reject));
+}
+
+// if userId undefined, allow to create an account with username. Otherwise, alert
+async function createAccount(){
+    const userId = getUserId(triedUsername);
+    if userId === undefined {
+        signUp();
+        location.href = "../timetable/timetable.html"
+    } else {
+        alert("Username is already in use. Please enter a different username.")
+    }
+}
+
+
+
 
 
 
