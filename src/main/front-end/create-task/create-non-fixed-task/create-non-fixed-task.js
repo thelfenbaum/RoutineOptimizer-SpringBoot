2e6f4ac309ef119995d394eb4eaca541c9360a5e
thelf <thelfenbaum@gmail.com>
2021-12-08 00:06:14 -0500

    renamed create-fixed-task.js to better reflect functionality. added create-non-fixed-task.js which is just a copy of create-fixed-task.js and needs editing

---
+++ src/main/front-end/create-task/create-non-fixed-task/create-non-fixed-task.js
@@ -0,0 +1,35 @@
+const API_BASE_URL = "http://localhost:8080/tasks/"
+
+let triedName = document.getElementById("name").value;
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+let raw = JSON.stringify({
+    "name": triedName,
+    "start_date_time": document.getElementById("start_date_time").value,
+    "duration": document.getElementById("duration").value,
+    "is_complete": document.getElementById("is_complete").value,
+    "user_id": document.getElementById("user_id").value
+});
+
+let requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+};
+
+async function getWeekSer(userid) {
+    return fetch(API_BASE_URL + "weeks/" + userid)
+        .then(response => response.json())
+}
+
+async function getTaskSers(userid) {
+    return fetch(API_BASE_URL + "tasks/" + userid)
+        .then(response => response.json())
+}
+
+async function importWeek() {
+    window.weekSer = getWeekSer(window.userid);
+    window.taskSers = getTaskSers(window.userid);
+    location.href = "../timetable/timetable.html";
