2872f443346e23ac0099ecc2f1658e38685eb5df
Runyu Xia <runyu.xia@mail.utoronto.ca>
2021-12-07 22:42:38 -0500

    Added method saveTaskSerializable

--- src/main/java/com/csc207/api/TaskController.java
+++ src/main/java/com/csc207/api/TaskController.java
@@ -2,25 +2,33 @@ package com.csc207.api;
 
 import com.csc207.domain.TaskSerializable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
 public class TaskController {
     @Autowired
     private final TaskSerializableInteractorDataOut taskSerializableInteractorDataOut;
+    private final TaskSerializableInteractorDataIn taskSerializableInteractorDataIn;
 
-    public TaskController(TaskSerializableInteractorDataOut taskSerializableInteractorDataOut){
+    public TaskController(TaskSerializableInteractorDataOut taskSerializableInteractorDataOut, TaskSerializableInteractorDataIn taskSerializableInteractorDataIn){
         this.taskSerializableInteractorDataOut = taskSerializableInteractorDataOut;
+        this.taskSerializableInteractorDataIn = taskSerializableInteractorDataIn;
     }
 
     @GetMapping("/tasks/{userid}")
     @CrossOrigin
     public List<TaskSerializable> getTasks(@PathVariable String userid){
         return this.taskSerializableInteractorDataOut.getTasksByUserId(Long.valueOf(userid));
+    }
+
+
+    @PostMapping("/tasks")
+    @CrossOrigin
+    @Transactional
+    public void saveTask(TaskSerializable task){
+        this.taskSerializableInteractorDataIn.saveTaskSerializable(task);
     }
 }
