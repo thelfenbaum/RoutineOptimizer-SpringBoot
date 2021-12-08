06bac8d16d632198b29f8daa0967fb57c42c8169
Runyu Xia <runyu.xia@mail.utoronto.ca>
2021-12-07 23:13:11 -0500

    Added importWeek to TaskController

--- src/main/java/com/csc207/api/TaskController.java
+++ src/main/java/com/csc207/api/TaskController.java
@@ -1,10 +1,14 @@
 package com.csc207.api;
 
+import com.csc207.domain.SerializableToWeekAdapter;
 import com.csc207.domain.TaskSerializable;
+import com.csc207.domain.Week;
+import com.csc207.domain.WeekSerializable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,6 +16,7 @@ public class TaskController {
     @Autowired
     private final TaskSerializableInteractorDataOut taskSerializableInteractorDataOut;
     private final TaskSerializableInteractorDataIn taskSerializableInteractorDataIn;
+    private WeekSerializableInteractorDataOut weekSerializableInteractorDataOut;
 
     public TaskController(TaskSerializableInteractorDataOut taskSerializableInteractorDataOut, TaskSerializableInteractorDataIn taskSerializableInteractorDataIn){
         this.taskSerializableInteractorDataOut = taskSerializableInteractorDataOut;
@@ -31,4 +36,18 @@ public class TaskController {
     public void saveTask(TaskSerializable task){
         this.taskSerializableInteractorDataIn.saveTaskSerializable(task);
     }
+
+
+
+    public Week importWeek(long userId) {
+        Week week;
+        WeekSerializable weekSers = this.weekSerializableInteractorDataOut.getWeekSerializableByUserId(userId);
+        this.weekSerializableInteractorDataOut.removeWeekSerializableByUserId(userId);
+        ArrayList<TaskSerializable> tasksSers = this.taskSerializableInteractorDataOut.getTasksByUserId(userId);
+        this.taskSerializableInteractorDataOut.removeTaskSerializablesByUserId(userId);
+        week = SerializableToWeekAdapter.SerializableToWeek(weekSers, tasksSers);
+        return week;
+    }
+
+
 }
