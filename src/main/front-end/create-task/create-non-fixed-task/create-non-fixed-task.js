acc0b04d1ed14c501a8f9242167dc4f57ec41a40
laviealon <79278018+laviealon@users.noreply.github.com>
2021-12-08 22:03:00 -0500

    Finished HTML and JS for the process for create-fixed-task. Ready to start testing and debugging.

--- src/main/front-end/create-task/create-non-fixed-task/create-non-fixed-task.js
+++ src/main/front-end/create-task/create-non-fixed-task/create-non-fixed-task.js
@@ -1,23 +1,5 @@
 const API_BASE_URL = "http://localhost:8080/tasks/"
 
-// let triedName = document.getElementById("name").value;
-// let myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
-//
-// let raw = JSON.stringify({
-//     "name": triedName,
-//     "start_date_time": document.getElementById("start_date_time").value,
-//     "duration": document.getElementById("duration").value,
-//     "user_id": document.getElementById("user_id").value
-// });
-//
-// let requestOptions = {
-//     method: 'POST',
-//     headers: myHeaders,
-//     body: raw,
-//     redirect: 'follow'
-// };
-
 async function createNonFixedTask(name, dueDateTime, duration, userid){
     let xhr = new XMLHttpRequest();
     xhr.open("POST", API_BASE_URL + "instantiate-non-fixed-task", true);
@@ -30,11 +12,6 @@ async function createNonFixedTask(name, dueDateTime, duration, userid){
         "isComplete": false
     }));
 }
-
-// async function getWeekSer(userid) {
-//     return fetch(API_BASE_URL + "weeks/" + userid)
-//         .then(response => response.json())
-// }
 
 async function getTaskSers(userid) {
     return fetch(API_BASE_URL + userid)
